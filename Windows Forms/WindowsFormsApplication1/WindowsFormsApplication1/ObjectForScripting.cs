b5ad3e56a47c0c3e3928166b8c2ee0fc184657fd
Leonardo Rojas <leorojas86@gmail.com>
2016-06-23 21:16:32 -0600

    Test concept

--- Windows Forms/WindowsFormsApplication1/WindowsFormsApplication1/ObjectForScripting.cs
+++ Windows Forms/WindowsFormsApplication1/WindowsFormsApplication1/ObjectForScripting.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Data.SQLite;
 
 namespace WindowsFormsApplication1
 {
@@ -9,6 +10,22 @@ namespace WindowsFormsApplication1
         public void sendMessage(string text)
         {
             MessageBox.Show(text);
+            TestSQLite();
+        }
+
+        private void TestSQLite()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            SQLiteConnection connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            connection.Open();
+            string sql = "CREATE TABLE highscores (name VARCHAR(20), score INT)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+
+            string sql2 = "insert into highscores (name, score) values ('Me', 9001)";
+            SQLiteCommand command2 = new SQLiteCommand(sql2, connection);
+            command.ExecuteNonQuery();
         }
     }
 }
