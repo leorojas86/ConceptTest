838228b7ef493fa682b798b422911a147258fee2
Leonardo Rojas <leorojas86@gmail.com>
2016-06-24 23:02:19 -0600

    Test concept

---
+++ Windows Forms/WindowsFormsApplication1/WindowsFormsApplication1/WebApplication/Service.js
@@ -0,0 +1,28 @@
+//Singleton instance
+var ServiceClient = { instance: new ServiceClientClass() };
+
+//Variables
+//ServiceClientClass.prototype._templateVariable = null;
+
+//Constructors
+function ServiceClientClass()
+{
+}
+
+//Methods
+ServiceClientClass.prototype.initialize = function()
+{
+    window.external.SQLiteCreateFile("MyDatabase.sqlite");
+    window.external.SQLiteOpenConnection("Data Source=MyDatabase.sqlite;Version=3;");
+    window.external.SQLiteExecuteNonQuery("CREATE TABLE users (name VARCHAR(20), password VARCHAR(20))");
+    window.external.SQLiteExecuteNonQuery("INSERT INTO users (name, password) values ('Test', 123)");
+};
+
+ServiceClientClass.prototype.login = function(name, password)
+{
+    var query = "SELECT name, password FROM users WHERE name = '" + name + "' AND password = '" + password + "'";
+    var resultString = window.external.SQLiteExecuteSelect(query);
+    var result = JSON.parse(resultString);
+
+    return result.rows.length;
+};
